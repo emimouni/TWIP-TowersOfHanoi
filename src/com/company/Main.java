a2eb5e863806fa5a99098c8433aec27b04bbdb13
Elias Mimouni <emimouni10@hotmail.com>
2017-01-22 12:49:58 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	
+        TowerOfHanoi hanoi = new TowerOfHanoi();System.out.print("Enter number of discs: ");
+        Scanner scanner = new Scanner(System.in);
+        int discs = scanner.nextInt();
+        hanoi.solve(discs, "1", "2", "3");
     }
 }
