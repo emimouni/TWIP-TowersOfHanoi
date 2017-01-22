8ff824aa61278ddfcd70cbb9ad555131203b1a46
Elias Mimouni <emimouni10@hotmail.com>
2017-01-22 12:50:40 -0500

    Update TowersOfHanoi.java

--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -1,8 +1,17 @@
 package com.company;
 
 /**
- * Created by em343 on 1/17/17.
+ * Created by emimo on 1/17/2017.
  */
-public class TowersOfHanoi {
+public class TowerOfHanoi {
 
+    public void solve(int discs, String a, String b, String c) {
+        if (discs == 1) {
+            System.out.println("Move one disk from tower " + a + " to " + c);
+        } else {
+            solve(discs - 1, a, c, b);
+            System.out.println("Move one disk from tower " + a + " to " + c);
+            solve(discs - 1, b, a, c);
+        }
+    }
 }
